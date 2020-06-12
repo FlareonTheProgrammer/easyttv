535c9f6a6d0345359119070b2d98c58b7cba6798
FlareonTheProgrammer <theinnovator64@gmail.com>
2020-06-12 10:37:58 -0500

    got rid of nameless function definition.

--- setup.js
+++ setup.js
@@ -30,36 +30,31 @@ let clientInfo = [
     cData.set("id", resp.id);
     cData.set("secret", resp.secret);
     console.log("Client data file created.");
-    (function () {
-      if (
-        fs.existsSync(`./json.sqlite`) &&
-        !fs.existsSync("../../json.sqlite")
-      ) {
-        console.log(
-          "[DEBUG] \x1b[36mjson.sqlite file found in easyttv module folder, attempting to move it...\x1b[0m"
+    if (fs.existsSync(`./json.sqlite`) && !fs.existsSync("../../json.sqlite")) {
+      console.log(
+        "[DEBUG] \x1b[36mjson.sqlite file found in easyttv module folder, attempting to move it...\x1b[0m"
+      );
+      try {
+        fs.renameSync("./json.sqlite", "../../json.sqlite");
+        return console.log(
+          "[SUCCESS] Moved json.sqlite to project root folder."
         );
-        try {
-          fs.renameSync("./json.sqlite", "../../json.sqlite");
-          return console.log(
-            "[SUCCESS] Moved json.sqlite to project root folder."
-          );
-        } catch (err) {
-          console.error(
-            "[ERROR] \x1b[33mFailed to move json.sqlite. Reason:\x1b[0m",
-            err
-          );
-        }
-      } else if (
-        fs.existsSync("./json.sqlite") &&
-        fs.existsSync("../../json.sqlite")
-      ) {
-        return console.warn(
-          "[WARN] \x1b[33mjson.sqlite file found in (what is assumed to be) the root folder, refusing to overwrite it due to potential for data loss.\x1b[0m"
+      } catch (err) {
+        console.error(
+          "[ERROR] \x1b[33mFailed to move json.sqlite. Reason:\x1b[0m",
+          err
         );
-      } else {
-        return console.warn("[WARN]\x1b[33m No json.sqlite file found.\x1b[0m");
       }
-    });
+    } else if (
+      fs.existsSync("./json.sqlite") &&
+      fs.existsSync("../../json.sqlite")
+    ) {
+      return console.warn(
+        "[WARN] \x1b[33mjson.sqlite file found in (what is assumed to be) the root folder, refusing to overwrite it due to potential for data loss.\x1b[0m"
+      );
+    } else {
+      return console.warn("[WARN]\x1b[33m No json.sqlite file found.\x1b[0m");
+    }
   } else if (beginSetup.proceed === "No") {
     return console.log(
       "Got it. You'll eventually have to do this in order for easyTTV to work."
